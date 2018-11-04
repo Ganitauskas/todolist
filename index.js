eb03ba15e634ad052427a3ef3871d8d9afcfba4f
Tadas Ganitauskas <tadas.ganitauskas@gmail.com>
2018-11-04 18:02:33 +0000

    added module schema

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express     =   require("express"),
     mongoose    =   require("mongoose"),
+    port        =   process.env.PORT || 3000,
     app         =   express();
     
 app.get('/', function(req, res){
-    
+    res.json({
+        message: "Hi from js object"
+    });
 });
     
-app.listen(process.env.PORT, function(){
+app.listen(port, function(){
     console.log('App is running on port: ' + process.env.PORT);
 });
